39196b773b8bfeb594fd4a7b8d90515f8216cee2
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-11-05 17:22:00 -0300

    finalização lista-alinos

--- lista-alunos.php
+++ lista-alunos.php
@@ -1,11 +1,21 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO(dsn: 'sqlite:' . $databasePath);
 
 $statement = $pdo->query('SELECT * FROM students;');
-$studentList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
+
+foreach ($studentDataList as $studentData) {
+    $studentList[] = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birth_date']));
+}
 
 var_dump($studentList);
