0edfcc78728cc41e683775ede5fc7a8eb8ebca11
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-11-11 16:41:15 -0300

    feat: add nova tabela phones

--- conexao.php
+++ conexao.php
@@ -1,7 +1,28 @@
 <?php
 
-$pdo = \Alura\Pdo\Infrastructure\Persistence\ConnectionCreator::createConnection();
+// $pdo = \Alura\Pdo\Infrastructure\Persistence\ConnectionCreator::createConnection();
+
+$caminhoBanco = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $caminhoBanco);
 
 echo "Conectei";
 
-$pdo->exec("CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);");
+// $pdo->exec("CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);");
+
+$createTableSql = '
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    birth_date TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS phones (
+    id INTEGER PRIMARY KEY,
+    area_code TEXT,
+    number TEXT,
+    student_id INTEGER,
+    FOREIGN KEY(student_id) REFERENCES students(id)
+    );
+';
+
+$pdo->exec($createTableSql);
--- src/Domain/Model/Student.php
+++ src/Domain/Model/Student.php
@@ -2,11 +2,15 @@
 
 namespace Alura\Pdo\Domain\Model;
 
+use Phone;
+
 class Student
 {
     private ?int $id;
     private string $name;
     private \DateTimeInterface $birthDate;
+    /** @var Phone[] */
+    private array $phones = [];
 
     public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
     {
@@ -49,5 +53,16 @@ class Student
         return $this->birthDate
             ->diff(new \DateTimeImmutable())
             ->y;
+    }
+
+    public function addPhone(Phone $phone): void
+    {
+        $this->phones[] = $phone;
+    }
+
+    /** @return Phone[] */
+    public function phones(): array
+    {
+        return $this->phones;
     }
 }
